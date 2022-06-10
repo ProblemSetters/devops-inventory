74681769b659d4463cbcd91ee5e1f05186588f0d
Mikhail Ryzhikov <i@developer.com.ua>
2022-06-10 05:12:36 +0300

    Update HackerSoft.java

--- HackerSoft.java
+++ HackerSoft.java
@@ -1,1 +1,14 @@
+class HackerSoft{
+  public static void main(String args[]) {
+    String licenseToken = System.getenv("HACKERSOFT_LICENSE_TOKEN");
+    String hostname = System.getenv("HOSTNAME");
+    Runtime.Version version = Runtime.version();
 
+    System.out.printf(
+      "%s@%s:%d\n",
+      licenseToken,
+      hostname,
+      version.feature()
+    );
+  }
+}
